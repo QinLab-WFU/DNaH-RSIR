06de24cf7ff021530a1a93ead48d6c89e13b1134
QinLab-WFU <128210944+QinLab-WFU@users.noreply.github.com>
2026-03-16 22:37:47 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-If the paper is accepted, the complete code will be uploaded soon.
+# Deep Neighbor-aware Hashing with Global-Local Representation for Multi-Label Remote Sensing Image Retrieval
+This paper is accepted for publication with Signal Processing: Image Communication.
+# Usage
+### Install dependencies:
+We use python to build our code, you need to install those package to run
+- Python 3.9.7
+- Pytorch 1.12.1
+- torchvision 13.1
+- CUDA 11.3
+
+### Processing dataset
+You should download three data sets including [UC Merced](http://weegee.vision.ucmerced.edu/datasets/landuse.html), 
+[AID](https://captain-whu.github.io/AID/), and MLRSNet and put data set in the corresponding directory under `data`. 
+If you want to construct your own training set and testing set, you should modify the path of images of training set, 
+testing set and database respectively in the `data/.../train.txt` , `data/.../test.txt` and `data/.../database.txt` for corresponding data set.
+
+## Training
+All parameters are defined in the train.py file, and the test methods are integrated into the train.py file. Therefore, it is only necessary to run
+python train.py
+
+### Download pretrained model
+Pretrained model is resnet50.pth.
--- main.py
+++ main.py
@@ -11,39 +11,38 @@ import centroids_generator
 import scipy.io as scio
 from timm.utils import AverageMeter
 from loguru import logger
-
-
-
+from models import NET_WTConv as NET
 
 def parse_option():
 
-    parser = argparse.ArgumentParser('argument for DNaH')
+    parser = argparse.ArgumentParser('argument for DNaH-RSIR')
     parser.add_argument('--device', type=str, default='cuda:0',
                         help='Device to use for computation (default: cuda:0)')
-    parser.add_argument('--info', type=str, default='DAQH-CSAM+Ada4元组+(a = 1 b = 3 c = 1 wt_levels=2!)',
+    parser.add_argument('--info', type=str, default='DNaH-RSIR',
                         help='name')
+    # a:eli  b: k
     parser.add_argument('--batch_size', type=int, default=64,
-                        help='batch size (default: 32)')
-    parser.add_argument('--epochs', type=int, default=20,
-                        help='epochs (default: 100)')
+                        help='batch size (default: 64)')
+    parser.add_argument('--epochs', type=int, default=200,
+                        help='epochs (default: 200)')
+
     parser.add_argument('--learning_rate', type=float, default=0.0001,
                         help='learning rate (default: 0.0001)')
     parser.add_argument('--weight_decay', type=float, default=5e-4,
                         help='weight decay (default: 5e-4)')
-    # parser.add_argument('--momentum', type=float, default=0.9,
-    #                     help='momentum (default: 0.9)')
+
     parser.add_argument('--betas', type=float, default=(0.9, 0.999),
                         help='betas (default: (0.9, 0.999))')
-    parser.add_argument('--n_class', type=int, default=60,
+    parser.add_argument('--n_class', type=int, default=17,
                         help='number of class (default: UCMD:17 MLRS:60 DFC15:8)')
     parser.add_argument("--topk", type=int, default=None, help="mAP@topk")
     parser.add_argument('--bit', type=int, choices=[16, 32, 48, 64, 128], default=128,
                         help='bit of hash code (choose from 16, 32, 48; default: 16)')
-    parser.add_argument('--dataset', type=str, default='MLRS',###
+    parser.add_argument('--dataset', type=str, default='UCMD',###
                         help='remote sensing dataset')
-    parser.add_argument('--pre_weight', default='/home/admin01/桌面/CXR/06-work/DAHNet-main/preweight/resnet50.pth',
+    parser.add_argument('--pre_weight', default='.../preweight/resnet50.pth',
                         help='path of pre-training weight of AlexNet')
-    parser.add_argument('--root_path', default='/home/admin01/桌面/CXR/SCFR-main/data/MLRS/',
+    parser.add_argument('--root_path', default='.../data/UCMD/',
                         help='root directory where the dataset is placed')
     parser.add_argument('--save_path', default='',
                         help='path where the result is placed')
@@ -56,18 +55,14 @@ def parse_option():
     parser.add_argument('--grama', type=float, default=0.1,
                         help='scheduler (default: 0)')
     parser.add_argument('--seed', type=int, default=0, help="random seed")
-
     parser.add_argument('--init-centroids-method', default='M', choices=['N', 'U', 'B', 'M', 'H'],
                         help='N = sign of gaussian; '
                              'B = bernoulli; '
-                             'M = MaxHD'
-                             'H = Hadamard matrix')
-    parser.add_argument('--alpha', type=float, default=0.5,
-                        help='weight of Hyp_loss (default:0.5)')
+                             'M = MaxHD')
     parser.add_argument('--alpha', type=float, default=0.5, help='weight of Hyp_loss (default:0.5)')
-
     parser.add_argument('--retrieve', type=int, default=0, help="retrieval number")
 
+    # 自适应四元组
     parser.add_argument("--type_of_distance", type=str, default="cosine", help="cosine/euclidean/squared_euclidean")
     parser.add_argument("--type_of_quadruplets", type=str, default="all", help="all/semi-hard/hard")
     parser.add_argument("--what_is_hard", type=str, default="one", help="all/one")
@@ -75,19 +70,22 @@ def parse_option():
     parser.add_argument("--k_delta", type=float, default=3, help="b(r)  K_Δ of Eq. (7) in paper")
 
     parser.add_argument("--save_dir", type=str, default="./Output", help="directory to output results")
+
+    # fix_margin_四元组
+    parser.add_argument('--margin', type=float, default=0.5, help='weight of margin (default: 0.5)')
     args = parser.parse_args()
     return args
-
-
 
 def prediction(loader, net):
     outputs = []
     labels = []
 
     for i, (images, lbls) in enumerate(loader):
+        start = time.time()
         images = images.cuda()
-        lbls = lbls.cuda()  # 将labels移到GPU
+        lbls = lbls.cuda()
         b, local_f, cls, cls1, cls2, cls3 = net(images)
+
         outputs.append(b.detach().cpu())
         labels.append(lbls.detach().cpu())
 
@@ -116,7 +114,7 @@ def _dataset(dataset,retrieve):
 
     elif dataset == 'UCMD':
         if retrieve == 0:
-            retrieve = 2100
+            retrieve = 378
         num_classes = 17
         trainset = ImageList(open('./data/UCMD/train.txt', 'r').readlines(), transform=data_transform['train'])
         testset = ImageList(open('./data/UCMD/test.txt', 'r').readlines(), transform=data_transform['val'])
@@ -124,11 +122,12 @@ def _dataset(dataset,retrieve):
 
     elif dataset == 'DFC15':
         if retrieve == 0:
-            retrieve = 3342
+            retrieve = 602
         num_classes = 8
         trainset = ImageList(open('./data/DFC15/train.txt', 'r').readlines(), transform=data_transform['train'])
         testset = ImageList(open('./data/DFC15/test.txt', 'r').readlines(), transform=data_transform['val'])
         database = ImageList(open('./data/DFC15/database.txt', 'r').readlines(), transform=data_transform['val'])
+
     if dataset == 'MLRS':
         num_classes = 60
         if retrieve == 0:
@@ -138,7 +137,6 @@ def _dataset(dataset,retrieve):
         testset = MLRS('./data/', 'query', transform=data_transform['val'])
         database = MLRS('./data/', 'retrieval', transform=data_transform['val'])
     num_train, num_test,num_database = len(trainset),len(testset),len(database)
-
     dsets = (trainset,testset,database)
     nums = (num_train, num_test,num_database)
     return nums, dsets,retrieve, num_classes
@@ -163,20 +161,18 @@ def main():
     num_train, num_test, num_database = nums
     dset_train, dset_test, dset_database = dsets
 
-    net = DAHNET.dahnet(code_length=args.bit, num_classes=args.n_class, feat_size=2048,
+    net = NET.dahnet(code_length=args.bit, num_classes=args.n_class, feat_size=2048,
                            device=args.device, pretrained=True)
 
     net.cuda()
 
-
-
     from loss.AdaQuadrupletLoss import AdaQuadrupletLoss
     criterion = AdaQuadrupletLoss(args, need_cnt=True)
+
     weight = load_preweights(net, preweights=args.pre_weight)
     net.load_state_dict(weight)
 
     optimizer = torch.optim.Adam([{'params': net.parameters(), 'lr': args.learning_rate}
-                                 # {'params': net.get_hash_params(), 'lr': 0.0001},
                                  ],
                                  # momentum=0.9,
                                  betas=(0.9, 0.999),
@@ -187,10 +183,7 @@ def main():
     best_map = 0  # best map
     best_epoch = 0  # best epoch
     total_time = 0
-    total_loss = 0
-    train_loss = []
-    mAPs = []
-    Time = []
+
 
     quadruplet_meter = AverageMeter()
     loss_meter = AverageMeter()
@@ -198,8 +191,11 @@ def main():
     cross = nn.CrossEntropyLoss()
     cross_loss = AverageMeter()
 
+
+
     for epoch in range(args.epochs):
         net.train()
+
         trainloader = DataLoader(dataset=dset_train,
                                  batch_size=64,
                                  shuffle=True,
@@ -208,40 +204,43 @@ def main():
         for i, (images, labels) in enumerate(trainloader):
             start = time.time()
             optimizer.zero_grad()
-            batch_x = images.cuda()
-            batch_y = labels.cuda()
-
 
             net = net.to(device)
             images = images.to(device)
             labels = labels.float()
             labels = labels.to(device)
-
             y_hat, local_f, cls, cls1, cls2, cls3 = net(images)
 
-            cls_loss = (1.0 / 2.0) * cross(cls, labels) + \
-                        (1.0 / 6.0) * (cross(cls1, labels) + cross(cls2, labels) + cross(cls3, labels))
+            sim_cls = cls @  cls.T
+            sim_cls1 = cls1 @ cls.T
+            sim_cls2 = cls2 @ cls2.T
+            sim_cls3 = cls3 @ cls3.T
+            sim_labels = labels @ labels.T
 
-            # 自适应四元组
+            cls_loss = (1.0 / 2.0) * cross(sim_cls, sim_labels) + \
+                       (1.0 / 6.0) * (cross(sim_cls1, sim_labels) + cross(sim_cls2, sim_labels) + cross(sim_cls3, sim_labels))
+
             loss, n_quadruplets = criterion(y_hat, labels)
             quadruplet_meter.update(n_quadruplets)
             if n_quadruplets == 0:
                 continue
+
             loss_meter.update(loss.item())
+
             c = 1
             Loss = loss + c*cls_loss
             loss_meter.update(Loss.item())
+
             optimizer.zero_grad()
-
             Loss.backward()
-            optimizer.step()
 
+
+            optimizer.step()
 
             total_time += time.time() - start
             sys.stdout.write('\r')
             sys.stdout.write('| Epoch [%3d/%3d] Iter[%3d/%3d] \t Dataset：%s \t Bit: %d \t\tLoss: %.4f  \t Time: %.4f  '
                              % (epoch, args.epochs, i, num_train, args.dataset,args.bit,Loss.item(),total_time))
-            # print(proxies)
             if save_flag:
                 f.write('| Epoch [' +
                         str(epoch) +
@@ -261,15 +260,12 @@ def main():
                         + str(total_time) +
                         '\n')
         scheduler.step()
-
         '''
                 training procedure finishes, evaluation
                 '''
 
         net.eval()
-        # save_dir = '/home/admin01/桌面/论文代码/SCFR-main/PR_curve/28/MLRS/'
-        # os.makedirs(save_dir, exist_ok=True)
-        save_model = '' + args.dataset +str(args.bit)
+        save_model = '../Result/model_pth' + args.dataset +str(args.bit)
         os.makedirs(save_model, exist_ok=True)
         testloader = DataLoader(dataset=dset_test,
                                 batch_size=64,
@@ -282,8 +278,11 @@ def main():
                                     num_workers=0)
         if train_flag or not os.path.exists(path + '.data'):
             with torch.no_grad():
+
                 data_predict, data_label = prediction(databaseloader, net)
+
                 test_predict, test_label = prediction(testloader, net)
+
 
             if not train_flag and save_flag:
                 datafile = open(path + '.data', 'w')
@@ -301,9 +300,8 @@ def main():
             test_label = np.array(data[3])
             print('------------- load data -------------')
 
-
-        data_predict = np.sign(data_predict)
-        test_predict = np.sign(test_predict)
+        data_predict = data_predict
+        test_predict = test_predict
         similarity = 1 - np.dot(test_predict, data_predict.T) / args.bit
         sim_ord = np.argsort(similarity, axis=1)
 
@@ -327,7 +325,6 @@ def main():
             if save_flag:
                 f.write("epoch: " + str(epoch) + '\n')
                 f.write("best_map: " + str(best_map) + '\n')
-                # torch.save(net.state_dict(), os.path.join(save_model, "-" + str(best_map) +"-"+ str(epoch) + ".pth"))
         else:
             print("epoch: ", epoch)
             print("map: ", mAP)
@@ -353,6 +350,7 @@ def main():
         scio.savemat(
             os.path.join(args.save_dir, args.info + args.dataset+"_"+str(best_map) + "_"+str(args.bit) + ".mat"),
             result_dict)
+
 if __name__ == "__main__":
 
     main()
